370be31a2233af7ab352315dd2492a5a01dadcbd
facundocabral <facucabral0212@gmail.com>
2022-08-20 16:33:50 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    window.()
-
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
