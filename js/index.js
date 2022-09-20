996d94950fcaecb470ba80388ba01cefa92a4dfe
facu2 <facucabral0212@gmail.com>
2022-09-20 18:32:45 -0300

    Entregable 4 Terminado

--- js/index.js
+++ js/index.js
@@ -13,6 +13,24 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
     Username = localStorage.getItem("UserMail");  //Tomamos del localStorage el value = mail q pone usuario.
-    document.getElementById("nombreUsuario").innerHTML=`${Username}`;
+    document.getElementById("nombreUsuario").innerHTML=`
+ 
+
+<div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" role="button" id="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+  ${Username}
+  </a>
+
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+  <li><a class="dropdown-item" href="cart.html">Mi Carrito</a></li>
+  <li><a class="dropdown-item" href="my-profile.html">Mi Perfil</a></li>
+  <li><a href="index.html">
+  <button onclick="cerrarSesion()" class="dropdown-item">Cerrar Sesión</button></a> </li>
+  </ul>
+</div>
+    `;
    
 });
+function cerrarSesion() {
+    localStorage.removeItem('UserMail');
+}
